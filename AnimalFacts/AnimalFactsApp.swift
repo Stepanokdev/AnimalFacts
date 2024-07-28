1e3110aaf34111c20b21186b005a74e9f167f8cf
stepanokdev <ivan.stepanok@raccoongang.com>
2024-07-28 17:37:05 +0300

    Initial Commit

---
+++ AnimalFacts/AnimalFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalFactsApp.swift
+//  AnimalFacts
+//
+//  Created by  Stepanok Ivan on 28.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimalFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
